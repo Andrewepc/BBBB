77bc7457221092d7f374b82f396051200add4d18
Andres Puente <00287919@uca.edu.sv>
2023-11-19 04:48:52 -0600

    feat: running and rotation

--- BBBB-client/Assets/Scripts/PlayerController.cs
+++ BBBB-client/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public CharacterController characterController;
 
     public float movementSpeed = 6;
+    public float jumpHeight = 10;
     public float gravity = 10f;
     
     private float fallingSpeed;
@@ -15,18 +16,21 @@ public class PlayerController : MonoBehaviour
     private byte actionStates = 0b00000000;
     private Dictionary<KeyCode, byte> inputsToByte = new Dictionary<KeyCode, byte>
     {
+        { KeyCode.Space, 0b00100000 },
         { KeyCode.W, 0b00001000 },
         { KeyCode.A, 0b00000100 },
         { KeyCode.S, 0b00000010 },
         { KeyCode.D, 0b00000001 }
     };
-    private Dictionary<byte, System.Func<byte, bool>> inputsToActions = new Dictionary<byte, System.Func<byte, bool>>
+    private Dictionary<byte, System.Func<byte, byte, bool>> inputsToActions = new Dictionary<byte, System.Func<byte, byte, bool>>
     {
-        { 4, (i) => { return ((i & 0b00001111) != 0); } },
-        { 3, (i) => { i &= 0b00001010; return ((i ^ 0b00001000) == 0); } },
-        { 2, (i) => { i &= 0b00000101; return ((i ^ 0b00000100) == 0); } },
-        { 1, (i) => { i &= 0b00001010; return ((i ^ 0b00000010) == 0); } },
-        { 0, (i) => { i &= 0b00000101; return ((i ^ 0b00000001) == 0); } }
+        
+        { 5, (i, a) => { return ((i & 0b00100000) != 0) && ((a & 0b11000000) == 0); } },
+        { 4, (i, a) => { return ((i & 0b00001111) != 0); } },
+        { 3, (i, a) => { i &= 0b00001010; return ((i ^ 0b00001000) == 0); } },
+        { 2, (i, a) => { i &= 0b00000101; return ((i ^ 0b00000100) == 0); } },
+        { 1, (i, a) => { i &= 0b00001010; return ((i ^ 0b00000010) == 0); } },
+        { 0, (i, a) => { i &= 0b00000101; return ((i ^ 0b00000001) == 0); } }
     };
     // Start is called before the first frame update
 
@@ -42,30 +46,49 @@ public class PlayerController : MonoBehaviour
     }
     private void UpdateStates()
     {
+        
         byte intendedActionStates = 0;
         for(int i = 7; i >= 0; i--)
         {
             intendedActionStates = (byte)(intendedActionStates << 1);
-            if (inputsToActions.ContainsKey((byte)i) && inputsToActions[(byte)i](lastInputStates)) intendedActionStates++;  
+            if (inputsToActions.ContainsKey((byte)i) && inputsToActions[(byte)i](lastInputStates, actionStates)) intendedActionStates++;  
         }
         actionStates = intendedActionStates;
     }
-    private void PhysicsUpdate()
+    private void UpdatePhysics()
     {
         Vector3 movingSpeed = new Vector3();
-        //Debug.Log(System.Convert.ToString(actionStates, 2).PadLeft(8, '0'));
+        Debug.Log(System.Convert.ToString(actionStates, 2).PadLeft(8, '0'));
         if ((actionStates & 0b00010000) != 0)
         {
             
             movingSpeed.x = (actionStates & 0b00000001) != 0 ? 1 : 0 + (actionStates & 0b00000100) != 0 ? -1 : 0;
             movingSpeed.z = (actionStates & 0b00001000) != 0 ? 1 : 0 + (actionStates & 0b00000010) != 0 ? -1 : 0;
         }
-        
+        if (movingSpeed.magnitude != 0) transform.rotation = Quaternion.LookRotation(movingSpeed);
         movingSpeed.Normalize();
         movingSpeed *= movementSpeed;
-        if (characterController.isGrounded) fallingSpeed = 0;
-        else fallingSpeed -= gravity * Time.fixedDeltaTime;
 
+        if (transform.position.y > 1.5)
+        {
+            fallingSpeed -= gravity * Time.fixedDeltaTime;
+            actionStates |= 0b01000000;
+        }
+        else 
+        {
+            fallingSpeed = 0;
+            actionStates &= 0b10111111;
+            
+            
+        }
+        
+        if ((actionStates & 0b00100000) != 0)
+        {
+            fallingSpeed = jumpHeight;
+            actionStates |= 0b01000000;
+        }
+        
+        
         characterController.Move(Time.fixedDeltaTime * (movingSpeed + transform.up * fallingSpeed));
     }
     void Start()
@@ -78,6 +101,6 @@ public class PlayerController : MonoBehaviour
     {
         CheckInputs();
         UpdateStates();
-        PhysicsUpdate();
+        UpdatePhysics();
     }
 }
