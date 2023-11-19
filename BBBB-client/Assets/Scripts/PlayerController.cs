d3fff1943623b20b16c275cc929299866482c980
Andres Puente <00287919@uca.edu.sv>
2023-11-19 08:25:45 -0600

    feat: run jump animations

--- BBBB-client/Assets/Scripts/PlayerController.cs
+++ BBBB-client/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController characterController;
-
+    public Animator animator;
     public float movementSpeed = 6;
     public float acceleration = 10;
     public float jumpHeight = 10;
@@ -40,6 +40,25 @@ public class PlayerController : MonoBehaviour
     };
     // Start is called before the first frame update
     Vector3 movingSpeed = new Vector3();
+
+    private int movingHash;
+    private int animSpeedHash;
+    private int velocityHash;
+    private int jumpingHash;
+    private int actionHash;
+    private int triggerNumHash;
+    
+    private void AnimatorHashSetup()
+    {
+        movingHash = Animator.StringToHash("Moving");
+        animSpeedHash = Animator.StringToHash("Animation Speed");
+        velocityHash = Animator.StringToHash("Velocity");
+        jumpingHash = Animator.StringToHash("Jumping");
+        actionHash = Animator.StringToHash("Action");
+        triggerNumHash = Animator.StringToHash("Trigger Number");
+        animator.SetFloat(animSpeedHash, 1);
+    }
+
     private void CheckInputs()
     {
         byte inputStates = 0b00000000;
@@ -71,12 +90,23 @@ public class PlayerController : MonoBehaviour
         facingDirection.z = (actionStates & 0b00001000) != 0 ? 1 : 0 + (actionStates & 0b00000010) != 0 ? -1 : 0;
         facingDirection.Normalize();
         facingSpeed = facingDirection * movementSpeed;
-        if ((actionStates & 0b00010000) != 0) facingSpeed *= sprintMult;
-        
+        if ((actionStates & 0b00010000) != 0) 
+        {
+            facingSpeed *= sprintMult;
+            animator.SetFloat(animSpeedHash, sprintMult);
+        }
+        else animator.SetFloat(animSpeedHash, 1);
+
         if (transform.position.y > 1.5)
         {
             fallingSpeed -= gravity * Time.fixedDeltaTime;
             actionStates |= 0b01000000;
+            if (fallingSpeed < 0 && animator.GetInteger(jumpingHash) != 2) { 
+                animator.SetInteger(jumpingHash, 2);
+                animator.SetInteger(triggerNumHash, 1);
+                animator.SetTrigger("Trigger");
+
+            }
         }
         else 
         {
@@ -85,6 +115,14 @@ public class PlayerController : MonoBehaviour
             if (movingSpeed.z < facingSpeed.z) movingSpeed.z += MathF.Min(Time.fixedDeltaTime * acceleration, facingSpeed.z - movingSpeed.z);
             else if (movingSpeed.z > facingSpeed.z) movingSpeed.z -= MathF.Min(Time.fixedDeltaTime * acceleration, movingSpeed.z - facingSpeed.z);
             if (movingSpeed.magnitude != 0) transform.rotation = Quaternion.LookRotation(movingSpeed);
+
+            if (fallingSpeed < 0)
+            {
+                animator.SetInteger(jumpingHash, 0);
+                animator.SetInteger(triggerNumHash, 1);
+                animator.SetTrigger("Trigger");
+            }
+                
 
             fallingSpeed = 0;
             actionStates &= 0b10111111;
@@ -97,15 +135,26 @@ public class PlayerController : MonoBehaviour
             if (Math.Abs(movingSpeed.z) < Math.Abs((facingDirection.z * movementSpeed))) movingSpeed.z = facingDirection.z * movementSpeed;
             
             actionStates |= 0b01000000;
+            animator.SetInteger(jumpingHash, 1);
+            animator.SetInteger(triggerNumHash, 1);
+            animator.SetTrigger("Trigger");
         }
         
         
         characterController.Move(Time.fixedDeltaTime * (movingSpeed + transform.up * fallingSpeed));
         playerCam.transform.position = transform.position + cameraOffset;
     }
+
+    private void UpdateAnimations()
+    {
+        animator.SetBool(movingHash, (actionStates & 0b00001111) != 0 && (actionStates & 0b11000000) == 0 && (actionStates & 0b00001010) != 10 && (actionStates & 0b00000101) != 5);
+        animator.SetFloat(velocityHash, movingSpeed.magnitude);
+
+        animator.SetInteger(actionHash, (actionStates & 0b00100000) != 0 ? 1 : 0);
+    }
     void Start()
     {
-        
+        AnimatorHashSetup();
     }
 
     // Update is called once per frame
@@ -114,5 +163,6 @@ public class PlayerController : MonoBehaviour
         CheckInputs();
         UpdateStates();
         UpdatePhysics();
+        UpdateAnimations();
     }
 }
